84a3b2e60acb914a171c20a7dd116be1dbb09974
Saba <sabatevdorashvili99@gmail.com>
2023-02-20 22:56:08 +0400

    post component and angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
